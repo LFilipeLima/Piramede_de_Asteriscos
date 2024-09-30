309f23b338bf7853263f3158cf96bc1f95748a30
LFilipe <lf44654@gmail.com>
2024-09-30 08:48:20 -0300

    Faz a piramide, precisa fazer um seletor de opcoes e colocar mais modelos

--- PyramidStar/src/App.java
+++ PyramidStar/src/App.java
@@ -1,6 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        
+        for (int i=0; i<45;i++){
+            for(int j=0;j<i;j++){
+                System.out.print('*');
+            }
+            System.out.print('\n');
+        }
     }
 }
